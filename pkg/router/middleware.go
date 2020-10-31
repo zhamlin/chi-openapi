6b8da376ef19593fb4384053abec7440172fa0b5
Zach Hamlin <zach@zhamlin.com>
2020-10-31 00:32:59 -0500

    feat:add verify response middleware and benchmarks

--- pkg/router/middleware.go
+++ pkg/router/middleware.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -21,7 +22,14 @@ type ErrorResponse struct {
 	Errors Errors `json:"errors"`
 }
 
-func Test(r *Router) func(http.Handler) http.Handler {
+func JSONHeader(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func VerifyRequest(r *Router) func(http.Handler) http.Handler {
 	router := openapi3filter.NewRouter()
 	if err := router.AddSwagger(r.swagger); err != nil {
 		panic(err)
@@ -83,6 +91,109 @@ func Test(r *Router) func(http.Handler) http.Handler {
 				}
 				return
 			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func wrapResponse(w http.ResponseWriter) wrapperWritter {
+	return wrapperWritter{
+		ResponseWriter: w,
+		body:           &bytes.Buffer{},
+	}
+}
+
+type wrapperWritter struct {
+	http.ResponseWriter
+	body       *bytes.Buffer
+	statusCode int
+}
+
+func (w *wrapperWritter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	// w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *wrapperWritter) Write(b []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	// writer := io.MultiWriter(w.ResponseWriter, w.body)
+	return w.body.Write(b)
+}
+
+func (w *wrapperWritter) Done() {
+	w.ResponseWriter.WriteHeader(w.statusCode)
+	w.ResponseWriter.Write(w.body.Bytes())
+}
+
+func VerifyResponse(r *Router) func(http.Handler) http.Handler {
+	router := openapi3filter.NewRouter()
+	if err := router.AddSwagger(r.swagger); err != nil {
+		panic(err)
+	}
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// TODO: cache data for already hit routes
+			rCtx := chi.RouteContext(r.Context())
+
+			pathParams := map[string]string{}
+			for i := 0; i < len(rCtx.URLParams.Values); i++ {
+				name := rCtx.URLParams.Keys[i]
+				pathParams[name] = rCtx.URLParams.Values[i]
+			}
+
+			route, _, err := router.FindRoute(r.Method, r.URL)
+			if err != nil {
+				fmt.Printf("%+v\n", err)
+				return
+			}
+
+			wrapped := wrapResponse(w)
+			next.ServeHTTP(&wrapped, r)
+
+			input := &openapi3filter.ResponseValidationInput{
+				Header: wrapped.Header(),
+				Status: wrapped.statusCode,
+				RequestValidationInput: &openapi3filter.RequestValidationInput{
+					Request:     r,
+					Route:       route,
+					QueryParams: r.URL.Query(),
+					PathParams:  pathParams,
+					Options: &openapi3filter.Options{
+						IncludeResponseStatus: true,
+					},
+				},
+			}
+			input.SetBodyBytes(wrapped.body.Bytes())
+			err = openapi3filter.ValidateResponse(r.Context(), input)
+			if err != nil {
+				if re, ok := err.(*openapi3filter.ResponseError); ok {
+					if se, ok := re.Err.(*openapi3.SchemaError); ok {
+						field := "unknown"
+						if p := se.JSONPointer(); len(p) > 0 {
+							field = p[0]
+						}
+						schemaError := ErrorResponse{Errors: Errors{Error{
+							Field:  field,
+							Reason: se.Reason,
+						}}}
+						_, err := json.Marshal(&schemaError)
+						if err != nil {
+							// TODO: log this?
+							panic(err)
+						}
+
+						// TODO: log this
+						// fmt.Printf("%+v\n", string(b))
+						w.WriteHeader(http.StatusInternalServerError)
+					}
+
+					// fmt.Printf("%+v\n", err)
+				}
+				return
+			}
+			wrapped.Done()
 		})
 	}
 }
--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -6,7 +6,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"testing"
+	"time"
 
 	. "chi-openapi/internal/testing"
 	. "chi-openapi/pkg/openapi/operations"
@@ -15,7 +18,9 @@ import (
 func dummyHandler(_ http.ResponseWriter, _ *http.Request) {}
 
 type Response struct {
-	Name string `json:"name"`
+	String string    `json:"string"`
+	Int    int       `json:"int" min:"3"`
+	Date   time.Time `json:"date"`
 }
 
 func TestRouterSimpleRoutes(t *testing.T) {
@@ -34,7 +39,15 @@ func TestRouterSimpleRoutes(t *testing.T) {
         "schemas": {
           "Response": {
             "properties": {
-              "name": {
+              "date": {
+                "format": "date-time",
+                "type": "string"
+              },
+              "int": {
+                "minimum": 3,
+                "type": "integer"
+              },
+              "string": {
                 "type": "string"
               }
             },
@@ -43,8 +56,8 @@ func TestRouterSimpleRoutes(t *testing.T) {
         }
       },
       "info": {
-          "title": "Title",
-          "version": "0.0.1"
+        "title": "Title",
+        "version": "0.0.1"
       },
       "openapi": "3.0.1",
       "paths": {
@@ -77,64 +90,228 @@ type InputBody struct {
 	SSN    string `json:"string" pattern:"^\\d{3}-\\d{2}-\\d{4}$"`
 }
 
-func TestRouterVerifyMiddleware(t *testing.T) {
+func TestRouterVerifyRequestMiddleware(t *testing.T) {
 	dummyR := NewRouter()
 	dummyR.Get("/", dummyHandler, []Option{
 		JSONBody("required data", InputBody{}),
 		JSONResponse(http.StatusOK, "OK", Response{}),
 	})
 
-	r := NewRouter().With(Test(dummyR))
+	r := NewRouter().With(VerifyRequest(dummyR))
 	r.Get("/", dummyHandler, []Option{
 		JSONBody("required data", InputBody{}),
 		JSONResponse(200, "OK", Response{}),
 	})
 
-	t.Run("invalid", func(t *testing.T) {
-		body, _ := json.Marshal(InputBody{Amount: 1})
-		req := httptest.NewRequest("GET", "/", bytes.NewReader(body))
-		req.Header.Add("Content-Type", "application/json")
-		w := httptest.NewRecorder()
-		r.ServeHTTP(w, req)
+	tests := []struct {
+		name   string
+		body   interface{}
+		method string
+		route  string
+		status int
+	}{
+		{
+			name:   "invalid",
+			body:   InputBody{Amount: 1},
+			method: "GET",
+			route:  "/",
+			status: http.StatusBadRequest,
+		},
+		{
+			name: "invalid ssn",
+			body: InputBody{
+				Amount: 3,
+				SSN:    "123-45-689",
+			},
+			method: "GET",
+			route:  "/",
+			status: http.StatusBadRequest,
+		},
+		{
+			name: "valid",
+			body: InputBody{
+				Amount: 3,
+				SSN:    "123-45-6789",
+			},
+			method: "GET",
+			route:  "/",
+			status: http.StatusOK,
+		},
+	}
 
-		resp := w.Result()
-		if expected := http.StatusBadRequest; resp.StatusCode != expected {
-			respBody, _ := ioutil.ReadAll(resp.Body)
-			t.Errorf("Expected %v, got %v:body:\n%v", expected, resp.StatusCode, string(respBody))
-		}
-	})
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var req *http.Request
+			if body := test.body; body != nil {
+				b, _ := json.Marshal(body)
+				req = httptest.NewRequest(test.method, test.route, bytes.NewReader(b))
+			} else {
+				req = httptest.NewRequest(test.method, test.route, nil)
+			}
+			req.Header.Add("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
 
-	t.Run("invalid ssn", func(t *testing.T) {
-		body, _ := json.Marshal(InputBody{
-			Amount: 3,
-			SSN:    "123-45-689",
+			resp := w.Result()
+			if expected := test.status; resp.StatusCode != expected {
+				respBody, _ := ioutil.ReadAll(resp.Body)
+				t.Errorf("Expected %v, got %v:body:\n%v", expected, resp.StatusCode, string(respBody))
+			}
 		})
-		req := httptest.NewRequest("GET", "/", bytes.NewReader(body))
-		req.Header.Add("Content-Type", "application/json")
-		w := httptest.NewRecorder()
-		r.ServeHTTP(w, req)
+	}
+}
 
-		resp := w.Result()
-		if expected := http.StatusBadRequest; resp.StatusCode != expected {
-			respBody, _ := ioutil.ReadAll(resp.Body)
-			t.Errorf("Expected %v, got %v:body:\n%v", expected, resp.StatusCode, string(respBody))
-		}
+func responseHandler(w http.ResponseWriter, r *http.Request) {
+	intQuery := r.URL.Query().Get("int")
+	if intQuery == "" {
+		intQuery = "1"
+	}
+	intValue, err := strconv.ParseInt(intQuery, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	response := Response{
+		Date: time.Now(),
+		Int:  int(intValue),
+	}
+	b, _ := json.Marshal(&response)
+	w.Write(b)
+}
+
+func TestRouterVerifyResponse(t *testing.T) {
+	dummyR := NewRouter()
+	dummyR.Get("/", dummyHandler, []Option{
+		JSONResponse(http.StatusOK, "OK", Response{}),
 	})
 
-	t.Run("valid", func(t *testing.T) {
-		body, _ := json.Marshal(InputBody{
-			Amount: 3,
-			SSN:    "123-45-6789",
+	r := NewRouter().
+		With(JSONHeader).
+		With(VerifyResponse(dummyR))
+	r.Get("/", responseHandler, []Option{
+		JSONBody("required data", InputBody{}),
+		JSONResponse(200, "OK", Response{}),
+	})
+
+	tests := []struct {
+		name   string
+		method string
+		route  string
+		status int
+		query  url.Values
+	}{
+		{
+			name:   "invalid",
+			method: "GET",
+			route:  "/",
+			status: http.StatusInternalServerError,
+		},
+		{
+			name:   "valid int",
+			method: "GET",
+			route:  "/",
+			status: http.StatusOK,
+			query: url.Values{
+				"int": []string{"3"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest(test.method, test.route+"?"+test.query.Encode(), nil)
+			req.Header.Add("Content-Type", "application/json")
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			resp := w.Result()
+			if expected := test.status; resp.StatusCode != expected {
+				respBody, _ := ioutil.ReadAll(resp.Body)
+				t.Errorf("Expected %v, got %v\nbody:\n%v", expected, resp.StatusCode, string(respBody))
+			}
 		})
-		req := httptest.NewRequest("GET", "/", bytes.NewReader(body))
-		req.Header.Add("Content-Type", "application/json")
-		w := httptest.NewRecorder()
-		r.ServeHTTP(w, req)
+	}
+}
 
-		resp := w.Result()
-		if expected := http.StatusOK; resp.StatusCode != expected {
-			respBody, _ := ioutil.ReadAll(resp.Body)
-			t.Errorf("Expected %v, got %v:body:\n%v", expected, resp.StatusCode, string(respBody))
+func BenchmarkRouter(b *testing.B) {
+	dummyR := NewRouter()
+	dummyR.Get("/", dummyHandler, []Option{
+		JSONResponse(http.StatusOK, "OK", Response{}),
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("Content-Type", "application/json")
+
+	b.Run("no verify middleware", func(b *testing.B) {
+		r := NewRouter().
+			With(JSONHeader)
+		r.Get("/", responseHandler, []Option{
+			JSONBody("required data", InputBody{}),
+			JSONResponse(200, "OK", Response{}),
+		})
+		b.ReportAllocs()
+		for n := 0; n < b.N; n++ {
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
 		}
 	})
+
+	b.Run("verify response", func(b *testing.B) {
+		r := NewRouter().
+			With(JSONHeader).
+			With(VerifyResponse(dummyR))
+		r.Get("/", responseHandler, []Option{
+			JSONBody("required data", InputBody{}),
+			JSONResponse(200, "OK", Response{}),
+		})
+		b.ReportAllocs()
+		for n := 0; n < b.N; n++ {
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+		}
+	})
+
+	input := InputBody{
+		Amount: 5,
+	}
+	data, err := json.Marshal(&input)
+	if err != nil {
+		b.Error(err)
+	}
+	req = httptest.NewRequest("GET", "/", bytes.NewReader(data))
+	req.Header.Add("Content-Type", "application/json")
+
+	b.Run("verify request", func(b *testing.B) {
+		r := NewRouter().
+			With(JSONHeader).
+			With(VerifyRequest(dummyR))
+		r.Get("/", responseHandler, []Option{
+			JSONBody("required data", InputBody{}),
+			JSONResponse(200, "OK", Response{}),
+		})
+
+		b.ReportAllocs()
+		for n := 0; n < b.N; n++ {
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+		}
+	})
+
+	b.Run("verify request and response", func(b *testing.B) {
+		r := NewRouter().
+			With(JSONHeader).
+			With(VerifyRequest(dummyR)).
+			With(VerifyResponse(dummyR))
+		r.Get("/", responseHandler, []Option{
+			JSONBody("required data", InputBody{}),
+			JSONResponse(200, "OK", Response{}),
+		})
+
+		b.ReportAllocs()
+		for n := 0; n < b.N; n++ {
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+		}
+	})
+
 }
