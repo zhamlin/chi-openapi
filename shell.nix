ae9ba185ed1b041a0cd5d9d38ce6df1894852c3d
Zach Hamlin <zach@zhamlin.com>
2020-11-16 23:10:57 -0600

    chore:cleanup shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 with import <nixpkgs> { };
 let
-  # define packages to install with special handling for OSX
   basePackages = [
     gnumake
     gcc
@@ -10,21 +9,9 @@ let
     goimports
     (callPackage ./nix/pkgs/enumer.nix { })
   ];
-  inputs = basePackages ++ lib.optional stdenv.isLinux inotify-tools;
 
+  inputs = basePackages ++ lib.optional stdenv.isLinux inotify-tools;
   shellHook = "";
-  # define shell startup command
-  # hooks = ''
-  #   # this allows mix to work on the local directory
-  #   mkdir -p .nix-mix
-  #   mkdir -p .nix-hex
-  #   export MIX_HOME=$PWD/.nix-mix
-  #   export HEX_HOME=$PWD/.nix-hex
-  #   export PATH=$MIX_HOME/bin:$PATH
-  #   export PATH=$HEX_HOME/bin:$PATH
-  #   export LANG=en_US.UTF-8
-  #   export ERL_AFLAGS="-kernel shell_history enabled"
-  # '';
 
 in mkShell {
   inherit shellHook;
